403882080a3c54fcccaf499b817e17ab5f727279
ishankkumar-007 <ishankkumar123@gmail.com>
2024-12-07 11:44:26 +0530

    feat: update interface startup script

--- others_home/script.sh
+++ others_home/script.sh
@@ -1,5 +1,5 @@
-roscore &
-sleep 5
-rosrun rosserial_arduino serial_node.py /dev/ttyACM0 &
-roslaunch depthai_ros_driver cam.launch &
+# roscore &
+# sleep 5
+rosrun rosserial_arduino serial_node.py /dev/ttyACM0 _baud:=9600 &
+# roslaunch depthai_ros_driver cam.launch &
 rosrun rose_tvmc_msg interface.py
